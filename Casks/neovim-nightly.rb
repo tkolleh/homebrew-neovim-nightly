b3f73119a27a248e4ac5b6737cc077ddecc7624a
TJ Kolleh <tinatuh@gmail.com>
2025-08-30 02:16:13 -0400

    fix: Remove livecheck

--- Casks/neovim-nightly.rb
+++ Casks/neovim-nightly.rb
@@ -10,13 +10,6 @@ cask "neovim-nightly" do
   desc "Vim-fork focused on extensibility and usability"
   homepage "https://neovim.io"
 
-  livecheck do
-    url "https://api.github.com/repos/neovim/neovim/releases/tags/nightly"
-    strategy :page_match do |page|
-      JSON.parse(page)["name"].scan(/nightly-(.*)/).first&.first
-    end
-  end
-
   binary "nvim-macos-#{arch}/bin/nvim"
 
   postflight do
